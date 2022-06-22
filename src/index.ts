a85e90c58ebe8e52970e2a3cf424fc9b883574bc
Art <ya.Artriches@ya.ru>
2022-06-22 21:42:41 +0300

    docs(readme): обновить readme

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 export { default as MoexClient } from "./client";
+
+import MoexClient from "./client";
+
+const moexClient = new MoexClient();
+
+(async () => {
+	const response = await moexClient.info.getHandbooks();
+
+	const a = response.data;
+	console.log(a);
+})();
