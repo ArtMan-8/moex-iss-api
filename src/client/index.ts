b0d870c6f490bf29f9f00dca56f1c50ea6fb8390
Art <ya.Artriches@ya.ru>
2022-07-01 22:11:54 +0300

    feat(client): реализовать произвольные запросы, согласно справочника MOEX ISS

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,19 +1,39 @@
 import { AxiosInstance } from "axios";
-import { createAxiosInstance } from "../api/index";
 
+import { createAxiosInstance } from "../api";
 import InfoApi from "./info";
-import { IMoexClient, IMoexClientOptions } from "./interfaces";
+import { IMoexClient, IMoexClientConstructor, IMoexConfig } from "./interfaces";
 
-/**
- * Базовые запросы к MOEX ISS API.
- * @class
- */
-export default class MoexClient implements IMoexClient {
+class MoexClient implements IMoexClient {
 	private axios: AxiosInstance;
 	public info: InfoApi;
 
-	constructor(config?: IMoexClientOptions) {
+	constructor(config?: IMoexConfig) {
 		this.axios = createAxiosInstance(config);
 		this.info = new InfoApi(this.axios);
 	}
+
+	public request = async (url: string) => {
+		return await this.axios.get(url.replace("/iss", ""));
+	};
 }
+
+/*-----------------------------------------------------------------*/
+
+function newMoexClient(
+	moexClientConstructor: IMoexClientConstructor,
+	config?: IMoexConfig,
+): IMoexClient {
+	return new moexClientConstructor(config);
+}
+
+const createMoexCLient =
+	(moexClient: IMoexClientConstructor) => (config?: IMoexConfig) =>
+		newMoexClient(moexClient, config);
+
+/**
+ * Базовые запросы к MOEX ISS API.
+ * {@link http://iss.moex.com/iss/reference/ MOEX ISS}
+ 
+ */
+export default createMoexCLient(MoexClient);
--- src/client/interfaces.ts
+++ src/client/interfaces.ts
@@ -1,13 +1,38 @@
-import { IAxiosConfig } from "src/api";
+import { AxiosResponse } from "axios";
 
 import InfoApi from "./info";
 
-export interface IMoexClientOptions extends IAxiosConfig {
-	[key: string]: any;
+export enum EResponseDataFormat {
+	json = "json",
+	csv = "csv",
+	html = "html",
+	xml = "xml",
 }
+
+export enum EResponseDataLanguage {
+	ru = "ru",
+	en = "en",
+}
+
+export interface IMoexConfig {
+	/** Язык ответа */
+	dataLanguage?: keyof typeof EResponseDataLanguage;
+	/** Формат ответа */
+	dataFormat?: keyof typeof EResponseDataFormat;
+}
+
+export type TResponse = (url: string) => Promise<AxiosResponse>;
 
 /** Базовые запросы к MOEX ISS API */
 export interface IMoexClient {
 	/** Получение различной информации */
 	info: InfoApi;
+	/** Произвольный запрос, согласно справочника
+	 * {@link http://iss.moex.com/iss/reference/ MOEX ISS}
+	 */
+	request: TResponse;
+}
+
+export interface IMoexClientConstructor {
+	new (config?: IMoexConfig): IMoexClient;
 }
