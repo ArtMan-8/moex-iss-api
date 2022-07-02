463186e0017bfd6744d8c4b2a4d21061ac5c33ca
Art <ya.Artriches@ya.ru>
2022-07-02 15:07:47 +0300

    chore(release): 0.3.0

--- src/client/info/requestTypes/turnoversColumns.ts
+++ src/client/info/requestTypes/turnoversColumns.ts
@@ -3,5 +3,5 @@ import { TAxiosResponse } from "src/api/interfaces";
 import { ITurnoversColumnsResponse } from "../responseTypes";
 
 export type TGetTurnoversColumns = () => Promise<
-	AxiosResponse<ITurnoversColumnsResponse>
+	TAxiosResponse<ITurnoversColumnsResponse>
 >;
